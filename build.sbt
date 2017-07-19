ae3574e2a64f85d0e34cf3720d3719110ba57298
Lukas Klingsbo <lukas.klingsbo@gmail.com>
2017-07-18 19:25:50 -0500

    Added mysql support and slick

--- build.sbt
+++ build.sbt
@@ -7,5 +7,8 @@ scalaVersion := "2.12.1"
 libraryDependencies ++= Seq(
   "com.typesafe.akka" %% "akka-http" % "10.0.9",
   "com.typesafe.akka" %% "akka-http-spray-json" % "10.0.9",
-  "com.typesafe.slick" %% "slick" % "3.2.0"
+  "com.typesafe.slick" %% "slick" % "3.2.0",
+  "org.slf4j" % "slf4j-nop" % "1.6.4",
+  "com.typesafe.slick" %% "slick-hikaricp" % "3.2.0",
+  "mysql" % "mysql-connector-java" % "6.0.6"
 )
--- src/main/scala/Mindlevel.scala
+++ src/main/scala/Mindlevel.scala
@@ -7,12 +7,17 @@ import akka.http.scaladsl.server.Directives._
 import akka.http.scaladsl.model.StatusCodes
 import akka.http.scaladsl.marshallers.sprayjson.SprayJsonSupport._
 import spray.json.DefaultJsonProtocol._
+import slick.jdbc.MySQLProfile.api._
+import com.typesafe.config._
 
 import scala.io.StdIn
 
 import scala.concurrent.Future
+import scala.concurrent.ExecutionContext.Implicits.global
 
 object Mindlevel {
+  val conf = ConfigFactory.load()
+  val db = Database.forConfig("db")
 
   // domain model
   final case class Item(name: String, id: Long)
