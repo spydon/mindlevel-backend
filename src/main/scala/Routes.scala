825eac8b61e4c22372fc9bc99c1504bfb0dffca9
Lukas Klingsbo <lukas.klingsbo@gmail.com>
2017-07-19 19:46:00 -0500

    Ranges for missions

--- src/main/scala/Routes.scala
+++ src/main/scala/Routes.scala
@@ -7,8 +7,8 @@ import akka.http.scaladsl.marshallers.sprayjson.SprayJsonSupport._
 import spray.json.DefaultJsonProtocol._
 import slick.jdbc.MySQLProfile.api._
 import com.typesafe.config._
-import net.mindlevel.models.Tables.{AccomplishmentRow, MissionRow, UserAccomplishmentRow, UserRow}
-import net.mindlevel.models.Tables.{Accomplishment, Mission, User, UserAccomplishment}
+import net.mindlevel.models.Tables.{AccomplishmentRow, MissionRow, UserRow}
+import net.mindlevel.models.Tables.{Accomplishment, Mission, User}
 import spray.json.{DeserializationException, JsNumber, JsValue, JsonFormat}
 
 import scala.concurrent.Future
@@ -31,14 +31,10 @@ object Routes {
   private implicit val accomplishmentFormat = jsonFormat7(AccomplishmentRow)
   private implicit val missionFormat = jsonFormat7(MissionRow)
   private implicit val userFormat = jsonFormat7(UserRow)
-  // Never used for explicit queries?
-  //private implicit val userAccomplishmentFormat = jsonFormat2(UserAccomplishmentRow)
-
 
   private val accomplishmentRoute =
     get {
       pathPrefix("accomplishment" / IntNumber) { id =>
-        // there might be no item for a given id
         val maybeAccomplishment = db.run(Accomplishment.filter(_.id === id).result.headOption)
 
         onSuccess(maybeAccomplishment) {
@@ -46,7 +42,7 @@ object Routes {
           case None => complete(StatusCodes.NotFound)
         }
       }
-    }// ~
+    } // ~
   // post {
   //   path("create-order") {
   //     entity(as[Order]) { order =>
@@ -60,22 +56,47 @@ object Routes {
   // }
 
   private val missionRoute =
-    get {
-      pathPrefix("mission" / IntNumber) { id =>
-        // there might be no item for a given id
-        val maybeMission = db.run(Mission.filter(_.id === id).result.headOption)
-
-        onSuccess(maybeMission) {
-          case Some(mission) => complete(mission)
-          case None => complete(StatusCodes.NotFound)
+    pathPrefix("mission") {
+      pathEndOrSingleSlash {
+        get {
+          complete(db.run(Mission.result))
         }
-      }
+      } ~
+        path(IntNumber) { id =>
+          get {
+            val maybeMission = db.run(Mission.filter(_.id === id).result.headOption)
+
+            onSuccess(maybeMission) {
+              case Some(mission) => complete(mission)
+              case None => complete(StatusCodes.NotFound)
+            }
+          }
+        } ~
+        path(Segment) { range =>
+          get {
+            if (range.contains("-")) {
+              val between = range.split("-")
+              val lower = between(0).toInt
+              val upper = between(1).toInt
+              val missions = db.run(Mission.filter(_.id >= lower).filter(_.id <= upper).result)
+              complete(missions)
+            } else if (range.contains(",")) {
+              val ids = range.split(",").map(_.toInt)
+              val query = for {
+                m <- Mission if m.id inSetBind ids
+              } yield m
+              val missions = db.run(query.result)
+              complete(missions)
+            } else {
+              complete(StatusCodes.BadRequest)
+            }
+          }
+        }
     }
 
   private val userRoute =
     get {
       pathPrefix("user" / Segment) { username =>
-        // there might be no item for a given id
         val maybeUser = db.run(User.filter(_.username === username).result.headOption)
 
         onSuccess(maybeUser) {
@@ -85,6 +106,6 @@ object Routes {
       }
     }
 
-  val all: Route = accomplishmentRoute ~ missionRoute ~ userRoute
+  def all: Route = accomplishmentRoute ~ missionRoute ~ userRoute
 
 }
