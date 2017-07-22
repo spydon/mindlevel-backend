c5e9b317ae7064b7b406a1e9817428937b76c2a2
Lukas Klingsbo <lukas.klingsbo@gmail.com>
2017-07-21 22:53:17 -0500

    Remove possibility to mixin implicits

--- src/main/scala/net/mindlevel/Boot.scala
+++ src/main/scala/net/mindlevel/Boot.scala
@@ -3,24 +3,19 @@ package net.mindlevel
 import akka.actor.ActorSystem
 import akka.http.scaladsl.Http
 import akka.stream.ActorMaterializer
-import com.typesafe.config.{Config, ConfigFactory}
 import net.mindlevel.routes.Routes
 
 import scala.concurrent.ExecutionContextExecutor
 import scala.io.StdIn
 
-trait Implicits {
-  private val configs = ConfigFactory.load()
-  private val actorSystemName = configs.getString("mindlevel.actorsystem")
+object Boot extends App {
   // needed to run the route
-  implicit val system = ActorSystem(actorSystemName)
+  implicit val system = ActorSystem()
   implicit val materializer = ActorMaterializer()
 
   // needed for the future map/flatmap in the end
   implicit val executionContext: ExecutionContextExecutor = system.dispatcher
-}
 
-object Mindlevel extends App with Implicits {
   val bindingFuture = Http().bindAndHandle(Routes.all, "localhost", 8080)
   println(s"Server online at http://localhost:8080/\nPress RETURN to stop...")
   StdIn.readLine() // let it run until user presses return
