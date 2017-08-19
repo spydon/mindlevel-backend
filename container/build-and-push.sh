93cc5fffebea5ab2c2d047e5289b2b2b27fecc29
Lukas Klingsbo <lukas.klingsbo@gmail.com>
2017-08-19 00:34:52 -0500

    Scala in container

--- container/build-and-push.sh
+++ container/build-and-push.sh
@@ -2,4 +2,4 @@
 docker build -t mindlevel . &&
 docker tag mindlevel:latest 589361660625.dkr.ecr.eu-central-1.amazonaws.com/mindlevel:latest &&
 $(aws ecr get-login --region eu-central-1) &&
-docker push 589361660625.dkr.ecr.eu-central-1.amazonaws.com/mindlevel:latest || echo "Failed"
+docker push 589361660625.dkr.ecr.eu-central-1.amazonaws.com/mindlevel:latest
