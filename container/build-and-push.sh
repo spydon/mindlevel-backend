6591de9f3fc3424b03235a054019663346e72313
Lukas Klingsbo <lukas.klingsbo@gmail.com>
2017-09-01 19:26:04 -0500

    Update to newest AWS login credentials command

--- container/build-and-push.sh
+++ container/build-and-push.sh
@@ -1,10 +1,14 @@
 #!/usr/bin/env bash
 ## Remember that the release branch is used
 cd `dirname "$0"` &&
+ping -c1 github.com &&
 rm -rf ./mindlevel-backend &&
 git clone -b release git@github.com:spydon/mindlevel-backend.git &&
-mysql -uroot -ppassword mindlevel < mindlevel-backend/mindlevel_schema.sql &&
+cd mindlevel-backend &&
+mysql -uroot -ppassword mindlevel < mindlevel_schema.sql &&
+sbt assembly &&
+cd .. &&
 docker build -t mindlevel . &&
 docker tag mindlevel:latest 589361660625.dkr.ecr.eu-central-1.amazonaws.com/mindlevel:latest &&
-$(aws ecr get-login --region eu-central-1) &&
+$(aws ecr get-login --region eu-central-1 --no-include-email) &&
 docker push 589361660625.dkr.ecr.eu-central-1.amazonaws.com/mindlevel:latest
