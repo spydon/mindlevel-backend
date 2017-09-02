dec2f55f0a5c4aeb57d8e4443331363d7f4cc54e
Lukas Klingsbo <lukas.klingsbo@gmail.com>
2017-09-02 15:03:32 -0500

    Syntax error in runner

--- container/runner.sh
+++ container/runner.sh
@@ -2,7 +2,7 @@
 # This is needed since only the EC2 instance has access to RDS
 # The build also has to be done here in that case...
 DB_HOST=mindlevel.cxkevz1h137d.eu-central-1.rds.amazonaws.com &&
-JDBC_HOST="jdbc:mysql//"$DB_HOST"/mindlevel" &&
+JDBC_HOST="jdbc:mysql://"$DB_HOST"/mindlevel" &&
 cd /root/mindlevel-backend &&
 mysql -uroot -ppassword -h $DB_HOST < mindlevel_schema.sql &&
 scala target/scala-2.12/mindlevel-backend-assembly-1.0.jar -Ddb.url=$JDBC_HOST
