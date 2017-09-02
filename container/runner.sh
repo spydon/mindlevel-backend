6bf277aeec64557078823126663386f7acc00b19
Lukas Klingsbo <lukas.klingsbo@gmail.com>
2017-09-02 12:01:32 -0500

    Correct environment passing switch

--- container/runner.sh
+++ container/runner.sh
@@ -2,6 +2,7 @@
 # This is needed since only the EC2 instance has access to RDS
 # The build also has to be done here in that case...
 export DB_HOST=mindlevel.cxkevz1h137d.eu-central-1.rds.amazonaws.com &&
+export JDBC_HOST="jdbc:mysql//"$DB_HOST"/mindlevel" &&
 cd /root/mindlevel-backend &&
 mysql -uroot -ppassword -h $DB_HOST < mindlevel_schema.sql &&
-scala target/scala-2.12/mindlevel-backend-assembly-1.0.jar -ddb.url=$DB_HOST
+scala target/scala-2.12/mindlevel-backend-assembly-1.0.jar -Ddb.url=$JDBC_HOST
