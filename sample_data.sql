07de8043caade6aff2e63e911054554a3525e472
Lukas Klingsbo <lukas.klingsbo@gmail.com>
2018-06-27 23:46:47 +0200

    Added base challenges

--- sample_data.sql
+++ sample_data.sql
@@ -1,23 +1,46 @@
 INSERT INTO user (username, description, image, score, created) 
-VALUES ("spydon", "Sample description", "1.jpg", 0, 0);
+VALUES ("Community", "To an artificial mind all reality is virtual", "4.jpg", 0, 0);
 
 INSERT INTO user_extra (username, password, email) 
-VALUES ("spydon", "$2a$10$dNvnLK3ZYSyjlwyyAc0Me.FbNvi2yEf6KPKKld62edoC8gChkVGpC", "me@lukas.fyi");
+VALUES ("Community", "$2a$10$dNvnLK3ZYSyjlwyyAc0Me.FbNvi2yEf6KPKKld62edoC8gChkVGpC", "me@lukas.fyi");
 
 INSERT INTO challenge (title, description, image, creator, validated)
-VALUES ("New challenge with extremely long title, is it fine?", "The description is actually also quite long how will all the views handle such a long description? I don't know, I guess we will find out!", "2.jpg", "spydon", True);
+VALUES (
+"Tree Climbing", 
+"Easy, just find a tree and climb as far up as you can!", "1.jpg", "Community", True);
 
 INSERT INTO challenge (title, description, image, creator, validated)
-VALUES ("Short and tall picture", "The description is short", "5.jpg", "spydon", True);
+VALUES (
+"Magic Marble",
+"When you get a magic marble, convince somebody that it is magic and tell them to do the same to somebody else", "2.jpg", "Community", True);
 
 INSERT INTO challenge (title, description, image, creator, validated)
-VALUES ("Medium length challenge title, it is fine.", "The description is actually also quite medium how will all the views handle such a long description? I don't know, I guess we will find out!", "5.jpg", "spydon", True);
+VALUES (
+"Cozy Corner",
+"Find a random corner and make it a cozy hangout, pillows and blankets for example", 
+"3.jpg", "Community", True);
 
-INSERT INTO accomplishment (title, description, image, challenge_id, created)
-VALUES ("Accomplishment Title!", "This was quite an accomplishment, wasn't it?", "2.jpg", 1, 0);
+INSERT INTO challenge (title, description, image, creator, validated)
+VALUES (
+"Face pimpin'", 
+"Paint your faces, glitter, flourescent colour or whatever you feel like!", 
+"4.jpg", "Community", True);
 
-INSERT INTO user_accomplishment (username, accomplishment_id)
-VALUES ("spydon", 1);
+INSERT INTO challenge (title, description, image, creator, validated)
+VALUES (
+"Take some illegal substance", 
+"What are you poppin' tonight?", "5.jpg", "Community", True);
 
-INSERT INTO session (username, session)
-VALUES ("spydon", null);
+INSERT INTO challenge (title, description, image, creator, validated)
+VALUES (
+"Sit-Dancing", 
+"Sit-dance on a dance floor for at least five minutes", "6.jpg", "Community", True);
+
+#INSERT INTO accomplishment (title, description, image, challenge_id, created)
+#VALUES ("", "", "2.jpg", 1, 0);
+#
+#INSERT INTO user_accomplishment (username, accomplishment_id)
+#VALUES ("Community", 1);
+#
+#INSERT INTO session (username, session)
+#VALUES ("Community", null);
