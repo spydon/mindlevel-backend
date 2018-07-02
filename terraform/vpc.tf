b5e1307a3b3435b33e7acef51af4ae82ae0bec62
Lukas Klingsbo <lukas.klingsbo@gmail.com>
2018-07-03 00:02:05 +0200

    Extend public sg for incomming to 8080

--- terraform/vpc.tf
+++ terraform/vpc.tf
@@ -87,6 +87,14 @@ resource "aws_security_group" "mindlevel_public_sg" {
   vpc_id = "${aws_vpc.mindlevel_vpc.id}"
 
   ingress {
+    from_port = 8080
+    to_port = 8080
+    protocol = "tcp"
+    cidr_blocks = [
+      "0.0.0.0/0"]
+  }
+
+  ingress {
     from_port = 22
     to_port = 22
     protocol = "tcp"
