53e014d842939f60e38c18f21f0ba7208697d9d5
Lukas Klingsbo <lukas.klingsbo@gmail.com>
2018-07-02 22:57:36 +0200

    Poor mans logging for troubleshooting aws

--- container/runner.sh
+++ container/runner.sh
@@ -5,4 +5,5 @@ DB_HOST=mindlevel.cxkevz1h137d.eu-central-1.rds.amazonaws.com &&
 JDBC_HOST="jdbc:mysql://"$DB_HOST"/mindlevel" &&
 cd /root/mindlevel-backend &&
 mysql -uroot -ppassword -h $DB_HOST < mindlevel_schema.sql &&
-scala target/scala-2.12/mindlevel-backend-assembly-1.0.jar -Ddb.url=$JDBC_HOST
+# TODO: Fix proper logging
+scala target/scala-2.12/mindlevel-backend-assembly-1.0.jar -Ddb.url=$JDBC_HOST &> mindlevel.log
