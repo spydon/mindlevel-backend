5384e7845061b5775a2ee62365c547bbccc95edb
Lukas Klingsbo <lukas.klingsbo@gmail.com>
2018-10-16 21:02:44 +0200

    Use correct schema variable for upgrade

--- database/upgrade.sh
+++ database/upgrade.sh
@@ -3,13 +3,13 @@
 
 SCHEMA=${2:-mindlevel}
 DB_HOST=$1
-echo "============================================================="
-echo " This will result in a few seconds of downtime for $SCHEMA   "
-echo "============================================================="
+echo "================================================================="
+echo " This will result in a few milliseconds of downtime for $SCHEMA   "
+echo "================================================================="
 cd "$(dirname "$0")" &&
 mysqldump -uroot -ppassword -h$DB_HOST --complete-insert --no-create-info $SCHEMA > ${SCHEMA}_backup.sql &&
 [ -s ${SCHEMA}_backup.sql ] &&
 mysqladmin -uroot -ppassword -h$DB_HOST -f drop $SCHEMA &&
 mysql -uroot -ppassword -h$DB_HOST < ${SCHEMA}_schema.sql &&
-mysql -uroot -ppassword -h$DB_HOST $SCHEMA < backup.sql &&
-echo "Successfully moved to new schema" || echo "Something failed"
+mysql -uroot -ppassword -h$DB_HOST $SCHEMA < ${SCHEMA}_backup.sql &&
+echo "Successfully moved to new schema" || (echo "Something failed" && false)
